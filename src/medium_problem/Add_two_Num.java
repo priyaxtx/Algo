d77a3145c0fff46645ad78715e9dca156df98f80
priyaxtx <priyatripathi44222@gmail.com>
2021-08-18 12:55:46 +0530

    New commit message

---
+++ src/medium_problem/Add_two_Num.java
@@ -0,0 +1,59 @@
+package medium_problem;
+public class Add_two_Num {
+	
+	static public class ListNode {
+	    int val;
+	    ListNode next;
+	    ListNode()
+	    {
+	      
+	        
+	    }
+	    ListNode(int val) 
+	    {
+	        this.val = val;
+	    }
+	    ListNode(int val, ListNode next) 
+	    {
+	        this.val = val;
+	        this.next = next;
+	    }
+	}
+	
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	
+	        int num=l1.size();
+	        int num1=l2.size();
+	         int val2,sum=0;
+	         int val3,sum1=0;
+	        if(num>=1){
+	       val2=l1.get(num);
+	       sum=sum*10+val2;
+	        num--;
+	        }
+	        if(num1>=1){
+	       val3=l1.get(num);
+	       sum1=sum1*10+val2;
+	        num--;
+	        }
+	        int SUM_OF_TWO_NUM=sum+sum1;
+	         ListNode l3=new ListNode();
+	        if(SUM_OF_TWO_NUM!=0)
+	        {
+	            int r=SUM_OF_TWO_NUM%10;
+	           l3.add(r);
+	        SUM_OF_TWO_NUM=SUM_OF_TWO_NUM/10;
+	        }
+	        return l3;
+	        
+	    }
+	
+
+public static void main(String a[])
+{
+	ListNode l1=new ListNode();
+	
+	
+}
+}
+
