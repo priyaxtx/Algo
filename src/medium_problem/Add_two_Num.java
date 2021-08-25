c21edde70ebe61c1f8be0c58c2f6abc07fb4d697
priyaxtx <priyatripathi44222@gmail.com>
2021-08-25 23:50:02 +0530

    nth change
    Change-Id: I14d72dd10b16227307198c2d9cdb8b0d69204d1d

--- src/medium_problem/Add_two_Num.java
+++ src/medium_problem/Add_two_Num.java
@@ -1,17 +1,22 @@
 package medium_problem;
+
+//import data_structure.LinkedList.Node;
+
 public class Add_two_Num {
-	
-	static public class ListNode {
+	int sum=0;
+	 public class ListNode {
 	    int val;
 	    ListNode next;
+	   
 	    ListNode()
 	    {
+	    	
 	      
-	        
 	    }
 	    ListNode(int val) 
 	    {
 	        this.val = val;
+	        sum=sum*10+val; 
 	    }
 	    ListNode(int val, ListNode next) 
 	    {
@@ -20,9 +25,9 @@ public class Add_two_Num {
 	    }
 	}
 	
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	   /* 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 	
-	        int num=l1.size();
+	    int num=l1.size();
 	        int num1=l2.size();
 	         int val2,sum=0;
 	         int val3,sum1=0;
@@ -35,8 +40,9 @@ public class Add_two_Num {
 	       val3=l1.get(num);
 	       sum1=sum1*10+val2;
 	        num--;
-	        }
-	        int SUM_OF_TWO_NUM=sum+sum1;
+	        }*/
+		
+	     /*   int SUM_OF_TWO_NUM=sum+sum1;
 	         ListNode l3=new ListNode();
 	        if(SUM_OF_TWO_NUM!=0)
 	        {
@@ -45,14 +51,23 @@ public class Add_two_Num {
 	        SUM_OF_TWO_NUM=SUM_OF_TWO_NUM/10;
 	        }
 	        return l3;
-	        
-	    }
+
+	    }	        */
+	 
+	 
+	 
+	 
 	
 
 public static void main(String a[])
 {
 	ListNode l1=new ListNode();
-	
+	ListNode head = new ListNode(1);
+	ListNode second = new ListNode(2);
+	ListNode third = new ListNode(3);
+
+	System.out.println(sum);
+	ListNode l2=new ListNode();
 	
 }
 }
