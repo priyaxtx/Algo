ed0ee7126317dbbe2426724bf61aeafebab8391b
priyaxtx <priyatripathi44222@gmail.com>
2021-08-31 03:42:14 +0530

    completed
    Change-Id: Iee41842a66b925c630557a7e142a0a73d19c7f9b

--- src/Easy_problem/Operation_wintout_sign.java
+++ src/Easy_problem/Operation_wintout_sign.java
@@ -1,5 +1,12 @@
 package Easy_problem;
-
+import java.util.Scanner;
 public class Operation_wintout_sign {
-
+public static void main(String ar[])
+{
+	Scanner scan=new Scanner(System.in);
+	System.out.println("enter two number:");
+	int num1=scan.nextInt();
+	int num2=scan.nextInt();
+	System.out.println(num1>num2?num1:num2);
+}
 }
